5a069598fd5ca20ca31df3769bf59b757e7e7c18
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-10-05 23:51:07 +0530

    updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,21 +7,23 @@ import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import AppliedJobTable from "./AppliedJobTable";
 import UpdateProfileDialoge from "./UpdateProfileDialoge";
+import { useSelector } from "react-redux";
 
-const skills = [
-  "Java",
-  "Html",
-  "CSS",
-  "JavaScript",
-  "ReactJs",
-  "Express.Js",
-  "Node.js",
-  "MongoDB",
-];
+// const skills = [
+//   "Java",
+//   "Html",
+//   "CSS",
+//   "JavaScript",
+//   "ReactJs",
+//   "Express.Js",
+//   "Node.js",
+//   "MongoDB",
+// ];
 const isResume = true;
 
 const Profile = () => {
   const [open, setOpen]= useState(false);
+  const {user} = useSelector(store=>store.auth);
   return (
     <div>
       <Navbar />
@@ -35,7 +37,7 @@ const Profile = () => {
               />
             </Avatar>
             <div>
-              <h1 className="font-medium text-xl">Full Name</h1>
+              <h1 className="font-medium text-xl">{user?.fullname}</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Consectetur, nobis?
@@ -49,18 +51,18 @@ const Profile = () => {
         <div className="my-5">
           <div className="flex items-center gap-3 my-2">
             <Mail />
-            <span>demo@gmail.com</span>
+            <span>{user?.email}</span>
           </div>
           <div className="flex items-center gap-3 my-2">
             <Contact />
-            <span>+91-7606976736</span>
+            <span>{user?.phoneNumber}</span>
           </div>
         </div>
         <div className="my-5">
           <h1>Skills</h1>
           <div className="flex items-center gap-1">
-            {skills.length != 0 ? (
-              skills.map((item, index) => <Badge key={index}>{item}</Badge>)
+            {user?.profile?.skills.length != 0 ? (
+              user?.profile?.skills.map((item, index) => <Badge key={index}>{item}</Badge>)
             ) : (
               <span>NA</span>
             )}
--- frontend/src/components/UpdateProfileDialoge.jsx
+++ frontend/src/components/UpdateProfileDialoge.jsx
@@ -10,7 +10,11 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { setUser } from '@/redux/authSlice'
+import { toast } from 'sonner'
 
 const UpdateProfileDialoge = ({ open, setOpen }) => {
   const [loading, setLoading] = useState(false);
@@ -24,11 +28,44 @@ const UpdateProfileDialoge = ({ open, setOpen }) => {
         skills: user?.profile?.skills?.map(skill => skill) || "",
         file: user?.profile?.resume || ""
     });
+    const dispatch = useDispatch();
     const changeEventHandler = (e)=>{
       setInput({...input, [e.target.name]: e.target.value});
     }
-    const submitHandler = (e) =>{
+    const fileChangeHandler = (e) =>{
+      const file = e.target.files?.[0];
+      setInput({...input, file});
+    }
+    const submitHandler = async (e) =>{
       e.preventDefault();
+      const formData = new FormData();
+        formData.append("fullname", input.fullname);
+        formData.append("email", input.email);
+        formData.append("phoneNumber", input.phoneNumber);
+        formData.append("bio", input.bio);
+        formData.append("skills", input.skills);
+        if (input.file) {
+          formData.append("file", input.file);
+      }
+      try {
+          setLoading(true);
+          const res = await axios.post(`${USER_API_END_POINT}/profile/update`, formData, {
+              headers: {
+                  'Content-Type': 'multipart/form-data'
+              },
+              withCredentials: true
+          });
+          if (res.data.success) {
+              dispatch(setUser(res.data.user));
+              toast.success(res.data.message);
+          }
+      } catch (error) {
+          console.log(error);
+          toast.error(error.response.data.message);
+      } finally{
+          setLoading(false);
+      }
+      setOpen(false);
       console.log(input);
     }
   return (
@@ -81,6 +118,7 @@ const UpdateProfileDialoge = ({ open, setOpen }) => {
                   id="file"
                   name="file"
                   type="file"
+                  onChange={fileChangeHandler}
                   accept="application/pdf"
                   className="col-span-3"
                 />
