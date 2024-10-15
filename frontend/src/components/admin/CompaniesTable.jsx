950d7268dd9a71437bf962354218268438972c22
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-10-15 15:24:38 +0530

    updated and created

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -11,8 +11,23 @@ import {
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Edit2, MoreHorizontal } from "lucide-react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const CompaniesTable = () => {
+  const { companies, searchCompanyByText } = useSelector(store => store.company);
+    const [filterCompany, setFilterCompany] = useState(companies);
+    const navigate = useNavigate();
+    useEffect(()=>{
+        const filteredCompany = companies.length >= 0 && companies.filter((company)=>{
+            if(!searchCompanyByText){
+                return true
+            };
+            return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase());
+
+        });
+        setFilterCompany(filteredCompany);
+    },[companies,searchCompanyByText])
   return (
     <div>
       <Table>
@@ -26,33 +41,33 @@ const CompaniesTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <tr>
-            <TableCell>
-              <Avatar>
-                <AvatarImage src="https://www.shutterstock.com/shutterstock/photos/2487834049/display_1500/stock-vector-growth-logo-technology-software-finance-investment-etc-logo-template-for-businesses-2487834049.jpg" />
-              </Avatar>
-            </TableCell>
-            {/* <TableCell>{company.name}</TableCell> */}
-            <TableCell>Company Name</TableCell>
-            {/* <TableCell>{company.createdAt.split("T")[0]}</TableCell> */}
-            <TableCell>13-10-2024</TableCell>
-            <TableCell className="text-right cursor-pointer">
-              <Popover>
-                <PopoverTrigger className="bg-white">
-                  <MoreHorizontal  />
-                </PopoverTrigger>
-                <PopoverContent className="w-32">
-                  <div
-                    // onClick={() => navigate(`/admin/companies/${company._id}`)}
-                    className="flex items-center gap-2 w-fit cursor-pointer"
-                  >
-                    <Edit2 className="w-4" />
-                    <span>Edit</span>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            </TableCell>
-          </tr>
+          {filterCompany?.map((company) => (
+            <tr>
+              <TableCell>
+                <Avatar>
+                  <AvatarImage src={company.logo} />
+                </Avatar>
+              </TableCell>
+              <TableCell>{company.name}</TableCell>
+              <TableCell>{company.createdAt.split("T")[0]}</TableCell>
+              <TableCell className="text-right cursor-pointer">
+                <Popover>
+                  <PopoverTrigger className="bg-white">
+                    <MoreHorizontal />
+                  </PopoverTrigger>
+                  <PopoverContent className="w-32">
+                    <div
+                      onClick={() => navigate(`/admin/companies/${company._id}`)}
+                      className="flex items-center gap-2 w-fit cursor-pointer"
+                    >
+                      <Edit2 className="w-4" />
+                      <span>Edit</span>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </TableCell>
+            </tr>
+          ))}
         </TableBody>
       </Table>
     </div>
