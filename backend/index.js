a6fc9f77d3e55171bc01c9ae5fe10a38108cb658
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-10-19 15:26:45 +0530

    Finally Completed

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,14 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-dotenv.config({});
+import path from "path";
 
+dotenv.config({});
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//directory Resolve method
+const _dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -28,6 +32,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+//connect to frontend
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
 app.listen(PORT, () => {
     connectDB();
