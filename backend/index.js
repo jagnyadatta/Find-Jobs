eae5db17a7f7ac7355fff881d20fdfccdaa0f7af
jagnyadatta <jagnyadattadalai99215@gmail.com>
2024-10-20 09:00:35 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://find-jobs-bv56.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
